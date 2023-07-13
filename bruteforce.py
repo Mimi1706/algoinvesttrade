ef0428232e762cbf60cdff496807088c35505f1a
Mimi <hana.nguyen@outlook.fr>
2023-07-13 10:26:34 +0200

    Cleaning functions

--- bruteforce.py
+++ bruteforce.py
@@ -24,28 +24,52 @@ actions = [
     {"name": "Action-20", "cost": 114, "profit": 0.18}
 ]
 
-combinations_list = []
-for r in range(1, len(actions) + 1):
-    combinations_list.extend(combinations(actions, r))
 
-valid_combinations = []
-for combination in combinations_list:
-    total_cost = sum(action["cost"] for action in combination)
-    if total_cost <= budget:
-        valid_combinations.append(combination)
+def combine_actions(actions):
+    combinations_list = []
+    for r in range(1, len(actions) + 1):
+        combinations_list.extend(combinations(actions, r))
+    return combinations_list
 
-best_combination = None
-total_cost = 0
-best_benefits = 0
 
-for combination in valid_combinations:
-    total_cost = sum(action["cost"] for action in combination)
-    total_benefits = sum(
-        action["cost"] * action["profit"] for action in combination)
-    if total_benefits > best_benefits:
-        best_benefits = total_benefits
-        best_combination = combination
+def keep_valid_combinations(combinations):
+    valid_combinations = []
+    for combination in combinations:
+        total_cost = sum(action["cost"] for action in combination)
+        if total_cost <= float(budget):
+            valid_combinations.append(combination)
+    return valid_combinations
 
-print("Best Combination:", best_combination)
-print("Total Cost:", total_cost)
-print("Total Benefits:", best_benefits)
+
+def find_best_combination(combinations):
+    best_combination = None
+    total_cost = 0
+    best_benefits = 0
+
+    for combination in combinations:
+        total_cost = sum(action["cost"] for action in combination)
+        total_benefits = sum(
+            action["cost"] * action["profit"] for action in combination)
+        if total_benefits > best_benefits:
+            best_benefits = total_benefits
+            best_combination = combination
+
+    print("Best Combination:", best_combination)
+    print("Total Cost:", total_cost)
+    print("Total Benefits:", best_benefits)
+
+
+def main():
+    find_best_combination(
+        keep_valid_combinations(combine_actions(actions)))
+
+
+if __name__ == '__main__':
+    import cProfile
+    import pstats
+    profiler = cProfile.Profile()
+    profiler.enable()
+    main()
+    profiler.disable()
+    stats = pstats.Stats(profiler).sort_stats('ncalls')
+    stats.print_stats()
--- optimized.py
+++ optimized.py
@@ -1,30 +1,74 @@
 import csv
 budget = 500
-actions = []
+actions = [
+    {"name": "Action-1", "price": 20, "profit": 5},
+    {"name": "Action-2", "price": 30, "profit": 10},
+    {"name": "Action-3", "price": 50, "profit": 15},
+    {"name": "Action-4", "price": 70, "profit": 20},
+    {"name": "Action-5", "price": 60, "profit": 17},
+    {"name": "Action-6", "price": 80, "profit": 25},
+    {"name": "Action-7", "price": 22, "profit": 7},
+    {"name": "Action-8", "price": 26, "profit": 11},
+    {"name": "Action-9", "price": 48, "profit": 13},
+    {"name": "Action-10", "price": 34, "profit": 27},
+    {"name": "Action-11", "price": 42, "profit": 17},
+    {"name": "Action-12", "price": 110, "profit": 9},
+    {"name": "Action-13", "price": 38, "profit": 23},
+    {"name": "Action-14", "price": 14, "profit": 1},
+    {"name": "Action-15", "price": 18, "profit": 3},
+    {"name": "Action-16", "price": 8, "profit": 8},
+    {"name": "Action-17", "price": 4, "profit": 12},
+    {"name": "Action-18", "price": 10, "profit": 14},
+    {"name": "Action-19", "price": 24, "profit": 21},
+    {"name": "Action-20", "price": 114, "profit": 18}
+]
 
-with open("./data/dataset2.csv", 'r') as file:
-    data = csv.DictReader(file)
-    actions = list(data)
 
-number_of_actions = len(actions)
+def read_csv(file):
+    with open(f"./data/{file}.csv", 'r') as file:
+        data = csv.DictReader(file)
+        actions = list(data)
+        return actions
 
-filtered_actions = [
-    action for action in actions if float(action['profit']) > 0 and float(action["price"]) > 0]
 
-sorted_actions = sorted(
-    filtered_actions, key=lambda action: float(action["profit"]), reverse=True)
+def knapsack(actions):
+    # remove null or negative profit actions
+    filtered_actions = [
+        action for action in actions if float(action['profit']) > 0 and float(action["price"]) > 0]
 
-selected_actions = []
-generated_profit = 0
-total_cost = 0
+    # sort the actions by highest profit (knapsack method)
+    sorted_actions = sorted(
+        filtered_actions, key=lambda action: float(action["profit"]), reverse=True)
+    return sorted_actions
 
-for action in sorted_actions:
-    if total_cost + float(action["price"]) <= budget:
-        selected_actions.append(action)
-        generated_profit += float(action["price"]) * \
-            (float(action["profit"])/100)
-        total_cost += float(action["price"])
 
-print("Selected Actions:", selected_actions)
-print("Total Profits:", generated_profit)
-print("Total Cost:", total_cost)
+def select_actions(actions):
+    selected_actions = []
+    generated_profit = 0
+    total_cost = 0
+
+    for action in actions:
+        if total_cost + float(action["price"]) <= float(budget):
+            selected_actions.append(action)
+            generated_profit += float(action["price"]) * \
+                (float(action["profit"])/100)
+            total_cost += float(action["price"])
+
+    print("Selected Actions:", len(selected_actions))
+    print("Total Profits:", generated_profit)
+    print("Total Cost:", total_cost)
+
+
+def main():
+    select_actions(knapsack(read_csv("dataset1.csv")))
+
+
+if __name__ == '__main__':
+    import cProfile
+    import pstats
+    profiler = cProfile.Profile()
+    profiler.enable()
+    main()
+    profiler.disable()
+    stats = pstats.Stats(profiler).sort_stats('ncalls')
+    stats.print_stats()
